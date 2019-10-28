2921a1c1b9f615b0a9f608f20d91a087ae5a4485
CharlesFord120 <charlieford130@gmail.com>
2019-10-28 10:31:22 -0600

    I tried so hard and got so far.

--- runtests.cpp
+++ runtests.cpp
@@ -34,76 +34,31 @@ void Assert(bool cond, std::string message)
 
 int main()
 {
-	// TODO:
-	// Write more tests to fully test your classes.
+	
+	// Testing IntStack
+	IntStack a;
+	IntStack b;
 
-	StringList a;
-	StringList b;
+	for(int i = 0; i <= 50; i++)
+		a.push(i);
+	for(int i = 50; i >= 0; i--)
+		b.push(i);
 
-	a.push_front("A");
-	Assert(a.front() == "A", "front == A");
-	a.push_front("B");
-	Assert(a.front() == "B", "front == B");
-	a.pop_front();
-	Assert(a.front() == "A", "front == A");
-	a.push_front("Z");
-	Assert(a.front() == "Z", "front == Z");
+	Assert(a.top() == 50, "top is 50");
+	Assert(b.top() == 0, "Top is 0");
+	a.pop();
+	b.pop();
+	Assert(a.top() == 49, "top is 49");
+	Assert(b.top() == 1, "Top is 1");
 
-	b = a;
-
-	Assert(b.front() == "Z", "b.front == Z");
-	b.pop_front();
-	Assert(b.front() == "A", "b.front == A");
-	b.pop_front();
-	Assert(b.empty() == true, "b is empty");
-	Assert(a.empty() == false, "a is NOT empty");
-
-	// TODO: check all methods on StringList...
-
-	StringStack c;
-	c.push("A");
-	c.push("B");
-	Assert(c.top() == "B", "B is at top");
-	c.pop();
-	Assert(c.empty() == false, "B is not empty");
-	c.pop();
-	Assert(c.empty() == true, "B is empty");
-
-	// TODO: check all methods on StringStack...
-
-	IntList ia;
-	ia.push_front(5);
-	ia.push_front(6);
-	Assert(ia.front() == 6, "front is 6");
-
-	// TODO: check all methods on IntList...
-
-	IntStack ic;
-	ic.push(6);
-	ic.push(5);
-	Assert(ic.top() == 5, "top is 5");
-
-	// TODO: check all methods on IntStack...
-	IntStack is;
-	for(int x = 0; x <= 50; x++)
-	{
-		is.push(x);
-	}
-	Assert(is.top() == 50, "Top is 50");
-	is.pop();
-	Assert(is.top() == 49, "Top is 49");
-	while(is.top() != 0)
-	{	
-		is.pop();
-	}
-	is.pop();
-	Assert(is.empty() == true, "is is empty");
-
-	IntQueue test;
-	test = {5,6,9,21,63};
-	test.pop();
-//	cout << test.front() << endl;
-	Assert(test.front() == 21, "test is true");
+	IntStack c;
+	c = a;
+	Assert (a.top() == c.top(), "a.top() == c.top()");
+	// Testing StringStack
+	
+	// Testing IntQueue
+	
+	// Testing StringQueue
 	return 0;
 }
 
